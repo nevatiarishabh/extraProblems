7fb7612aa007907d1bec089d36cf95649879961a
Ritika <nevatiaritika@gmail.com>
2018-08-14 23:07:39 +0530

    Add reverse String problem

--- src/ReverseExpression.java
+++ src/ReverseExpression.java
@@ -1,4 +1,32 @@
 
+
+/**
+Input will be a String - a valid expression.
+
+Expressions have operators and operands.
+Operators can be * or + or - or /
+Operands are valid numbers (from 10^-6 upto 10^6)
+
+Output should be a String with reversed expression
+
+Example:
+
+Input: "123+456/23*2"
+Output: "2*23/456+123"
+
+Note: Test for extreme cases.
+
+*/
 public class ReverseExpression {
+
+	public static void main(String args[]){
+		String sampleInput = ("123+456/23*2");
+		String sampleOutput = reverse(sampleInput);
+		System.out.println(sampleOutput);
+	}
+
+	public static String reverse(String input){
+		return "";
+	}
 
 }
